6c3c89215eda4a0053bdfc10c2cb28411f9ecc14
ErikVerta <89127095+ErikVerta@users.noreply.github.com>
2021-08-30 15:04:05 +0200

    Labb 1

--- Labb1/Labb1/Program.cs
+++ Labb1/Labb1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace Labb1
 {
     class Program
@@ -7,7 +8,7 @@ namespace Labb1
         static string _input;
         static int _firstIndex;
         static int _secondIndex;
-        static long _sumOfSubstrings = 0;
+        static long _sumOfNumbers = 0;
         static void Main(string[] args)
         {
             //Ber användaren om en sträng samt sparar ner denna sträng i Input.
@@ -18,11 +19,13 @@ namespace Labb1
             SearchForInteger();
             
             //Skriver ut totalen av alla substrängar alltså sumOfSubstrings.
-            Console.WriteLine($"Total = {_sumOfSubstrings}");
+            Console.WriteLine($"Total = {_sumOfNumbers}");
         }
 
-        //Denna metoden letar efter en siffra i strängen. Om den hittar en siffra så sparar den ner dess index i _firstIndex samt sparar ner vilken siffra det är i startOfSubstring.
-        //Letar efter nästa siffra som är likadan samt sparar ner dess index i _secondIndex. Kallar sedan på SearchForLetter metoden med substrängen som argument.
+        //Denna metoden letar efter en siffra i strängen. Om den hittar en siffra så sparar den ner vilken siffra det är samt dess index.
+        //Letar sedan vidare efter en likadan siffra, om den hittar en bokstav innan den hittar en likadan siffra så avbryter den loopen och går tillbaks till första loopen.
+        //Om den hittar en likadan siffra innan den hittar en bokstav så sparar den ner dess index samt sparar ner alla siffror mellan första och andra index.
+        //Sen plussar den på talet på _sumOfNumbers samt kallar på PrintString metoden med talet som argument.
         private static void SearchForInteger ()
         {          
             for (int i = 0; i < _input.Length; i++)
@@ -30,51 +33,36 @@ namespace Labb1
                 if (int.TryParse(_input.Substring(i, 1), out int number))
                 {
                     _firstIndex = i;
-                    string startOfSubstring = number.ToString();
+                    string startOfNumberString = number.ToString();
                     
                     for (int j = _firstIndex + 1; j < _input.Length; j++)
                     {
-                        if (_input.Substring(j, 1) == startOfSubstring)
+                        if (_input[j].ToString() == startOfNumberString)
                         {
                             _secondIndex = j;
-                            SearchForLetter(_input.Substring(_firstIndex, _secondIndex - _firstIndex + 1));
+                            string numberString = _input.Substring(_firstIndex, _secondIndex - _firstIndex + 1);
+                            _sumOfNumbers += long.Parse(numberString);
+                            PrintString(numberString);
+                            break;
+                        }
+                        else if (int.TryParse(_input.Substring(j, 1), out int x) == false )
+                        {
                             break;
                         }
                     }
                 }
             }
         }
-
-        //Denna metoden kollar så att substrängen inte innehåller någon bokstav.
-        //Om den inte innehåller någon bokstav så plussar den på talet på _sumOfSubstrings samt kallar på PrintString metoden med substrängen som argument.
-        //Om den innehåller en bokstav så återvänder den till SearchForInteger metoden.
-        private static void SearchForLetter (string subString)
-        {
-            for (int i = 0; i < subString.Length; i++)
-            {
-                if (int.TryParse(subString.Substring(i, 1), out int x))
-                {
-                    continue;
-                }
-                else
-                {
-                    return;
-                }
-            }
-            _sumOfSubstrings += long.Parse(subString);
-            PrintString(subString);
-        }
-        
-        //Denna metoden delar upp inputen i 3 delar. första delen är innan substrängen, andra delen är substrängen och tredje delen är resterande av inputen.
-        //Den skriver sedan ut dessa delar var för sig samt byter färg på substrängen till röd.
-        private static void PrintString(string subString)
+             
+        //Denna metoden delar upp inputen i 3 delar. första delen är innan talet, andra delen är talet och tredje delen är resterande av inputen.
+        //Den skriver sedan ut dessa delar var för sig samt byter färg på talet till röd.
+        private static void PrintString(string numberString)
         {
             string firstPartOfInput = _input.Substring(0, _firstIndex);
-            string secondPartOfInput = _input.Substring(_secondIndex + 1, _input.Length - (_firstIndex + subString.Length));
-       
+            string secondPartOfInput = _input.Substring(_secondIndex + 1);
             Console.Write(firstPartOfInput);                      
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write(subString);
+            Console.Write(numberString);
             Console.ForegroundColor = ConsoleColor.Gray;           
             Console.Write(secondPartOfInput);
             
